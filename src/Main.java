bbc136758b1051240cf7001aed7dedf81e8a8ea5
Yugi777 <147806229+Yugi777@users.noreply.github.com>
2024-06-18 09:40:38 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <5; i++)
+        {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(i+1);
+
+            }
+            System.out.println();
+        }
+
+        for (int k = 0; k <6 ; k++)
+        {
+            for (int j = 0; j <k ; j++) {
+                System.out.print(k);
+            }
+            System.out.println();
+
+        }
+
+        for (int w = 1; w <6 ; w++)
+        {
+            for (int v = 6; v >w ; v--) {
+                System.out.print(w);
+            }
+            System.out.println();
+
+        }
+            
+        }
+
+    }
